c9619111a22430b48751c8ac513f75f2a676cb03
defense <1900013009@pku.edu.cn>
2023-04-12 14:38:36 +0800

    config init

---
+++ config.py
@@ -0,0 +1,22 @@
+import argparse
+
+
+def get_config():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--path_src', type=str, default="../autodl-tmp/datasets_original",
+                        help='Path to the source folder containing original datasets.')
+    parser.add_argument('--path_tar', type=str, default='../autodl-tmp/datasets_keypoint',
+                        help='Path to the target folder to store the processed datasets.')
+    parser.add_argument('--save_name', type=str, default="best.pth",
+                        help='Name of the file to save the best model during training.')
+    parser.add_argument('--model', type=str, default='resnet18',
+                        help='Name of the model architecture to be used for training.')
+    parser.add_argument('--epoch', type=int, default=30,
+                        help='Number of epochs for training.')
+    parser.add_argument('--lr', type=float, default=1e-3,
+                        help='Learning rate for the optimizer during training.')
+    parser.add_argument('--early_stop', type=int, default=10,
+                        help='Number of epochs with no improvement to trigger early stopping.')
+
+    args = parser.parse_args()
+    return args
