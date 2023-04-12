df7cae37a46b6f3cb49573e4cde05761ea6d1a98
defense <1900013009@pku.edu.cn>
2023-04-12 14:10:13 +0800

    visual

--- utils_/function_.py
+++ utils_/function_.py
@@ -1,5 +1,5 @@
 import torch
-
+from visual_kit import visualize_result
 def train_epoch(model, optimizer, train_loader, loss_function, device):
     model.train()
     running_loss = 0.0
@@ -36,20 +36,14 @@ def val_epoch(model, val_loader, loss_function, device):
 
     return running_loss / len(val_loader)
 
-def test_epoch(model, test_loader, device):
+def test_epoch(model, test_loader,target_path, device):
     model.eval()
-    all_predictions = []
-    all_ground_truths = []
-
     with torch.no_grad():
         for inputs, targets in test_loader:
             inputs = inputs.to(device)
 
             outputs = model(inputs)
-            raise NotImplementedError("visualize please")
-            # predictions = extract_keypoints(outputs)  # You should implement the extract_keypoints function
-
-            # all_predictions.extend(predictions)
-            all_ground_truths.extend(targets.numpy())
-
-    return all_predictions, all_ground_truths
+            visualize_result(inputs,outputs,targets,
+                             target_path,model.output_format)
+    print("finish testing")
+    return 
---
+++ utils_/visual_kit.py
@@ -0,0 +1,51 @@
+import numpy as np
+import cv2
+def tensor_to_numpy_image(tensor, mean, std):
+    # Convert tensor to numpy array
+    np_img = tensor.numpy()
+    
+    # Rearrange channels from (C, H, W) to (H, W, C)
+    np_img = np.transpose(np_img, (1, 2, 0))
+    
+    # Denormalize the image using mean and standard deviation
+    np_img = (np_img * std) + mean
+    
+    # Clip the float values to [0, 1] and convert to uint8
+    np_img = np.clip(np_img, 0, 1)
+    np_img = (np_img * 255).astype(np.uint8)
+    
+    return np_img
+
+def visualize_result(image_tensor, result,
+                      ground_truth, target_path,
+                        ouput_format='regression'):
+    # Convert image to numpy array
+    
+    img_np = tensor_to_numpy_image(image_tensor)
+
+    # Draw ground truth points
+    for gt_point in ground_truth:
+        if ouput_format == 'regression':
+            x, y = gt_point
+        elif ouput_format == 'heatmap':
+            y, x = np.unravel_index(np.argmax(gt_point), gt_point.shape)
+        else:
+            raise ValueError('Invalid ouput_format. Choose "regression" or "heatmap".')
+
+        img_np = cv2.circle(img_np, (int(x), int(y)), 3, (0, 255, 0), -1)
+        img_np = cv2.putText(img_np, 'gt', (int(x) + 5, int(y) - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 0), 1)
+
+    # Draw predicted points
+    for pred_point in result:
+        if ouput_format == 'regression':
+            x, y = pred_point
+        elif ouput_format == 'heatmap':
+            y, x = np.unravel_index(np.argmax(pred_point), pred_point.shape)
+        else:
+            raise ValueError('Invalid ouput_format. Choose "regression" or "heatmap".')
+
+        img_np = cv2.circle(img_np, (int(x), int(y)), 3, (0, 255, 255), -1)
+        img_np = cv2.putText(img_np, 'predict', (int(x) + 5, int(y) + 5), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0, 255, 255), 1)
+
+    # Save the visualization
+    cv2.imwrite(target_path, cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR))
