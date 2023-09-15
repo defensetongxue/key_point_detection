4b170b9c5e071c309538f90dc6b57b74223f76b0
defense <1900013009@pku.edu.cn>
2023-09-15 14:13:18 +0800

    visual

--- test.py
+++ test.py
@@ -16,16 +16,14 @@ os.makedirs(result_path,exist_ok=True)
 print(f"the mid-result and the pytorch model will be stored in {result_path}")
 
 # Create the model and criterion
-model, criterion = get_instance(models, args.model,args.configs)
+model, criterion = get_instance(models, args.model,args.configs['model'])
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model.to(device)
 model.load_state_dict(
     torch.load(args.save_name))
 model.eval()
-
 # Create the dataset and data loader
-data_path=os.path.join(args.path_tar, "GY")
-test_dataset = CustomDatset(data_path,split='test')
+test_dataset = CustomDatset(args.data_path,args.configs,split='test')
 test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 # Create the visualizations directory if it doesn't exist
 
@@ -36,11 +34,12 @@ with torch.no_grad():
     for inputs, targets,meta in test_loader:
         inputs = inputs.to(device)
 
-        output = model(inputs)
+        output = model(inputs)[0].unsqueeze(0)
         score_map = output.data.cpu()
         preds = decode_preds(score_map)
         visualize_and_save_landmarks(
             image_path=meta[0],
+            image_resize=args.configs['image_resize'],
             preds=preds,
             save_path=os.path.join(visual_dir,os.path.basename(meta[0])))
     print("Finished testing")
--- utils_/visual_functional.py
+++ utils_/visual_functional.py
@@ -2,9 +2,9 @@ import torch
 import math
 import cv2
 
-def visualize_and_save_landmarks(image_path, preds, save_path,text=None):
+def visualize_and_save_landmarks(image_path,image_resize, preds, save_path,text=None):
     img = cv2.imread(image_path)
-    img=cv2.resize(img,(416,416))
+    img=cv2.resize(img,image_resize)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     # Ensure preds is a NumPy array with shape (98, 2)
     if isinstance(preds, torch.Tensor):
@@ -12,11 +12,11 @@ def visualize_and_save_landmarks(image_path, preds, save_path,text=None):
 
     # Draw landmarks on the image
     print(text)
-    for x, y in preds:
-        cv2.circle(img, (int(x), int(y)), 8, (255, 0, 0), -1)
-        if text:
-            cv2.putText(img,str(text),(200, 200),
-            fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(255, 0, 0),thickness=4)
+    x, y = preds
+    cv2.circle(img, (int(x), int(y)), 8, (255, 0, 0), -1)
+    if text:
+        cv2.putText(img,str(text),(200, 200),
+        fontFace=cv2.FONT_HERSHEY_SIMPLEX,fontScale=1,color=(255, 0, 0),thickness=4)
     # Save the image with landmarks
     cv2.imwrite(save_path, cv2.cvtColor(img, cv2.COLOR_RGB2BGR))
     
@@ -25,18 +25,18 @@ def get_preds(scores):
     get predictions from score maps in torch Tensor
     return type: torch.LongTensor
     """
-    assert scores.dim() == 4, 'Score maps should be 4-dim'
-    maxval, idx = torch.max(scores.view(scores.size(0), scores.size(1), -1), 2)
+    assert scores.dim() == 3, 'Score maps should be 3-dim'
+    maxval, idx = torch.max(scores.view(scores.size(0), -1), 1)
 
-    maxval = maxval.view(scores.size(0), scores.size(1), 1)
-    idx = idx.view(scores.size(0), scores.size(1), 1) + 1
+    maxval = maxval.view(scores.size(0), -1)
+    idx = idx.view(scores.size(0), -1) + 1
 
-    preds = idx.repeat(1, 1, 2).float()
+    preds = idx.repeat(1, 2).float()
 
-    preds[:, :, 0] = (preds[:, :, 0] - 1) % scores.size(3) + 1
-    preds[:, :, 1] = torch.floor((preds[:, :, 1] - 1) / scores.size(3)) + 1
+    preds[:, 0] = (preds[:,  0] - 1) % scores.size(2) + 1
+    preds[:, 1] = torch.floor((preds[:,  1] - 1) / scores.size(2)) + 1
 
-    pred_mask = maxval.gt(0).repeat(1, 1, 2).float()
+    pred_mask = maxval.gt(0).repeat(1, 2).float()
     preds *= pred_mask
     return preds
 
@@ -46,19 +46,15 @@ def decode_preds(output):
     coords = coords.cpu()
     # pose-processing
     for n in range(coords.size(0)):
-        for p in range(coords.size(1)):
-            hm = output[n][p]
-            px = int(math.floor(coords[n][p][0]))
-            py = int(math.floor(coords[n][p][1]))
-            if (px > 1) and (px < map_width) and (py > 1) and (py < map_height):
-                diff = torch.Tensor([hm[py - 1][px] - hm[py - 1][px - 2], hm[py][px - 1]-hm[py - 2][px - 1]])
-                coords[n][p] += diff.sign() *.25
+        hm = output[n]
+        px = int(math.floor(coords[n][0]))
+        py = int(math.floor(coords[n][1]))
+        if (px > 1) and (px < map_width) and (py > 1) and (py < map_height):
+            diff = torch.Tensor([hm[py - 1][px] - hm[py - 1][px - 2], hm[py][px - 1]-hm[py - 2][px - 1]])
+            coords[n] += diff.sign() *.25
     preds = coords.clone()
 
     # Transform back
-    if preds.dim() < 3:
-        preds = preds.view(1, preds.size())
-
     return preds*4 # heatmap is 1/4 to original image
 
 
