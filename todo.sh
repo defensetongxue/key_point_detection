d8c3cd3bd7a85dbba3ae263046dc5180facacfb3
guoyi <1900013009@pku.edu.cn>
2023-09-23 21:13:39 +0800

    bug

--- todo.sh
+++ todo.sh
@@ -2,4 +2,4 @@ python -u train.py --cfg ./config_file/hrnet_u.json
 python -u test.py --cfg ./config_file/hrnet_u.json
 python -u train.py --cfg ./config_file/hrnet_v.json
 python -u test.py --cfg ./config_file/hrnet_v.json
-python ting.py
+python ring.py
